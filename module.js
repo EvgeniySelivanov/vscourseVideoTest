6301e3837a2e25ea2486e2a401b33f9447d9b8e2
EvgeniySelivanov <genyaselivanovzp@gmail.com>
2021-11-28 13:12:11 +0200

    v2

---
+++ module.js
@@ -0,0 +1,19 @@
+alert("module");
+if (true) {
+  alert("true");
+}
+class Car {
+  constructor() {
+    super();
+  }
+  get() {
+    setTimeout(() => {
+      const obj = {
+        a: 1,
+        b: 2,
+      };
+    });
+  }
+}
+console.log('fdfggdg')
+
